048ab2e026b22630d88ebdf18072bee7b0a6fd79
nonaddict <jossefkisch@gmail.com>
2025-05-23 00:59:59 +0300

    new database

--- ebay-iphones/products.js
+++ ebay-iphones/products.js
@@ -1,7 +1,7 @@
 // products.js
 const products = async () => {
     try {
-      const response = await fetch('https://backend-q7ou.onrender.com/ebay-iphones/products', {
+      const response = await fetch('https://sql-backend-production-6b3b.up.railway.app/get-all-products/ebay_iphones', {
         method: 'GET',
         mode: 'cors'
       });
